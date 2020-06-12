fe7d18112ce1ddac26e0a05920fb7dfa3ac8b655
liyiwen <johnnyeven@gmail.com>
2020-06-12 19:56:30 +0800

    duration string

---
+++ pkg/timelib/duration_string.go
@@ -0,0 +1,93 @@
+package timelib
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type DurationString time.Duration
+
+var (
+	DurationStringZero = DurationString(0)
+)
+
+func ParseDurationStringFromString(s string) (ds DurationString, err error) {
+	var d time.Duration
+	d, err = time.ParseDuration(s)
+	ds = DurationString(d)
+	return
+}
+
+var _ interface {
+	sql.Scanner
+	driver.Valuer
+} = (*DurationString)(nil)
+
+func (ds *DurationString) Scan(value interface{}) error {
+	switch v := value.(type) {
+	case string:
+		d, err := ParseDurationStringFromString(v)
+		if err != nil {
+			return err
+		}
+		*ds = d
+	case nil:
+		*ds = DurationStringZero
+	default:
+		return fmt.Errorf("cannot sql.Scan() strfmt.DurationString from: %#v", v)
+	}
+	return nil
+}
+
+func (ds DurationString) Value() (driver.Value, error) {
+	return ds.String(), nil
+}
+
+func (ds DurationString) String() string {
+	return time.Duration(ds).String()
+}
+
+var _ interface {
+	encoding.TextMarshaler
+	encoding.TextUnmarshaler
+} = (*DurationString)(nil)
+
+func (ds DurationString) MarshalText() ([]byte, error) {
+	if ds.IsZero() {
+		return []byte(""), nil
+	}
+	str := ds.String()
+	return []byte(str), nil
+}
+
+func (ds *DurationString) UnmarshalText(data []byte) (err error) {
+	str := string(data)
+	if len(str) == 0 || str == "0" || str == "0s" {
+		str = DurationStringZero.String()
+	}
+	*ds, err = ParseDurationStringFromString(str)
+	return
+}
+
+func (ds DurationString) IsZero() bool {
+	return time.Duration(ds).Nanoseconds() == 0
+}
+
+var _ interface {
+	json.Marshaler
+	json.Unmarshaler
+} = (*DurationString)(nil)
+
+func (ds *DurationString) UnmarshalJSON(data []byte) (err error) {
+	str := string(data)
+	*ds, err = ParseDurationStringFromString(str)
+	return
+}
+
+func (ds DurationString) MarshalJSON() ([]byte, error) {
+	return []byte(ds.String()), nil
+}
