3ca4e9db2f64c48e552ceff532fff54c619f63d2
liyiwen <johnnyeven@gmail.com>
2020-06-23 18:23:44 +0800

    drone ci config file

--- internal/project/drone/platform.go
+++ internal/project/drone/platform.go
@@ -12,3 +12,13 @@ func (p *PipelinePlatform) WithOS(os enums.DroneCiPlatformOs) *PipelinePlatform 
 	p.OS = os
 	return p
 }
+
+func (p *PipelinePlatform) WithArchitecture(arch enums.DroneCiPlatformArch) *PipelinePlatform {
+	p.Architecture = arch
+	return p
+}
+
+func (p *PipelinePlatform) WithVersion(v int) *PipelinePlatform {
+	p.Version = v
+	return p
+}
