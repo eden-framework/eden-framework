ea79bcd1d4b7721a40c8e72283f2fca5c3976fa0
liyiwen <johnnyeven@gmail.com>
2020-09-24 16:13:30 +0800

    optimize project generator

--- pkg/application/options.go
+++ pkg/application/options.go
@@ -16,24 +16,33 @@ func WithApollo(conf *apollo.ApolloBaseConfig) Option {
 }
 
 func WithConfig(conf ...interface{}) Option {
-	configFieldNames := make(map[string]struct{})
-	for i, c := range conf {
-		typ := reflect.TypeOf(c)
-		if typ.Kind() != reflect.Ptr {
-			logrus.Panicf("the [%d] config must be a ptr value", i)
-		}
-
-		typ = reflectx.IndirectType(typ)
-		for j := 0; j < typ.NumField(); j++ {
-			field := typ.Field(j)
-			if _, ok := configFieldNames[field.Name]; ok {
-				logrus.Panicf("the [%d] config field named [%s] is duplicated. can not define the same field name in the root of each config struct.", i, field.Name)
-			} else {
+	return func(app *Application) {
+		configFieldNames := make(map[string]struct{})
+		for _, c := range app.envConfig {
+			typ := reflectx.IndirectType(reflect.TypeOf(c))
+			for j := 0; j < typ.NumField(); j++ {
+				field := typ.Field(j)
 				configFieldNames[field.Name] = struct{}{}
 			}
 		}
-	}
-	return func(app *Application) {
+
+		for i, c := range conf {
+			typ := reflect.TypeOf(c)
+			if typ.Kind() != reflect.Ptr {
+				logrus.Panicf("the [%d] config must be a ptr value", i)
+			}
+
+			typ = reflectx.IndirectType(typ)
+			for j := 0; j < typ.NumField(); j++ {
+				field := typ.Field(j)
+				if _, ok := configFieldNames[field.Name]; ok {
+					logrus.Panicf("the [%d] config field named [%s] is duplicated. can not define the same field name in the root of each config struct.", i, field.Name)
+				} else {
+					configFieldNames[field.Name] = struct{}{}
+				}
+			}
+		}
+
 		app.envConfig = append(app.envConfig, conf...)
 	}
 }
